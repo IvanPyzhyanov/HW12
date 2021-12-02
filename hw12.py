4ed9703eed09c68f1c44f13827f46a07f3796660
IvanPyzhyanov <piv85@mail.ru>
2021-12-02 15:56:25 +0300

    Realising fourth step of homework

--- hw12.py
+++ hw12.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, request
 
 #loading data from candidates.json
 with open("candidates.json", "r", encoding="UTF-8") as file:
@@ -24,5 +24,30 @@ def candid_page(code):
 @app.route("/list/")
 def list_page():
     return render_template("list.html", candid=candidates)
+
+#making candidates names list
+names_list = []
+for i in candidates:
+    names_list.append(i["name"])
+
+print(settings["case-sensitive"])
+
+#making page for searching candidates by their names or just by letters in names (also we will use or not the case-sensitivity which depends on set in settings)
+@app.route("/search/")
+def search_page():
+    s = request.args.get("name")
+    if s is None:
+        return "Enter the name of candidate"
+    if settings["case-sensitive"] == True:
+        name_match = [name for name in names_list if s.lower() in name.lower()]
+    else:
+        name_match = [name for name in names_list if s in name]
+        return name_match
+    if len(name_match) == 0:
+        return "No candidates in list with entered name. Try another one."
+    else:
+        return render_template("search.html", names=name_match, candid=candidates)
+
+
 
 app.run()
